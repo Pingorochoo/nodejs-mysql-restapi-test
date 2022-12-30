2278d7ba3daf0845fa68b114bfb808d7fe3d4d1f
Pingorochoo <manuelalonsomartinezvargas@gmail.com>
2022-12-30 17:42:55 -0500

    add passowd as env var

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 import { createPool } from "mysql2/promise";
-import { DB_HOST, DB_DATABASE, DB_USER, DB_PORT } from './config.js'
+import { DB_HOST, DB_DATABASE, DB_USER, DB_PORT, DB_PASSWORD } from './config.js'
 const POOL = createPool(
     {
         host: DB_HOST,
         user: DB_USER,
         port: DB_PORT,
-        database: DB_DATABASE
+        database: DB_DATABASE,
+        password: DB_PASSWORD
     }
 )
 
