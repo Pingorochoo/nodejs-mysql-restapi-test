7da127e41696c51d91d6888c51b0e5d14bfb4d78
Pingorochoo <manuelalonsomartinezvargas@gmail.com>
2022-12-30 17:26:09 -0500

    modify

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ app.use('/api', employeesRoutes)
 
 app.use((req,res,next)=>{
     res.status(404).json({
-        message:'endpoint not found'
+        message:'endpoint not found',
+        url:req.path
     })
 })
 
